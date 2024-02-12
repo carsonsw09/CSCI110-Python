2efec34b2c928221c5d595cf0f3061d3cc03ee90
Carson <carsonsw09@gmail.com>
2024-02-12 15:18:47 -0700

    Week 3 Hw Assignment is completed

---
+++ Hw#1/main.py
@@ -0,0 +1,27 @@
+#Made by Carson White 2/8/24
+#CSCI110-002-44680
+#Week 3 Hw Assignment problem 2.14 part 1
+
+A = "All"
+
+B = "work"
+
+C = "and"
+
+D = "no"
+
+E = "play"
+
+F = "makes"
+
+G = "Jack"
+
+H = "a"
+
+I = "dull"
+
+J= "boy"
+
+print(A,B,C,D,E,F,G,H,I,J)
+
+
