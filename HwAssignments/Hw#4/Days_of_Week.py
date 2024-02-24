c54bf4a7dd9591e9eef293e79bcd29b357ebac89
Carson <carsonsw09@gmail.com>
2024-02-24 16:33:24 -0700

    Homework 4 is now finished

--- HwAssignments/Hw#4/Days_of_Week.py
+++ HwAssignments/Hw#4/Days_of_Week.py
@@ -4,7 +4,9 @@
 
 import sys
 
-def Day_Converter(x):
+def Day_Converter(x):# This function checks what the input value is then
+                     # prints the day of the week based off the corresponding
+                     # input value
     if x == 0:
         print("The day of the week is Monday")
     elif x == 1:
@@ -19,7 +21,15 @@ def Day_Converter(x):
         print("The day of the week is Saturday")
     elif x == 6:
         print("The day of the week is Sunday")
+        
+    # The elif statements will check through the values in descending order until
+    # the input value is found, if not found, then there was an error with the user input
 
 if __name__ == '__main__':
     x = int(input("Please enter a integer from 0-6 and this program will return the day of the week: "))
+    
+    # The statement above stores the user input into the x-variable
+    
     Day_Converter(x)
+    
+    # Above the Day_Converter function is called passing the variable x into the function
